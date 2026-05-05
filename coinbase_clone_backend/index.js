922f2d08fd87ba6de800a0ba1520caaf15877175
Smeakme <susankrah2@gmail.com>
2026-05-05 17:46:24 +0000

    changes made

--- coinbase_clone_backend/index.js
+++ coinbase_clone_backend/index.js
@@ -16,6 +16,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "https://smeaks-crypto-app.netlify.app",
+  "https://smeaks-crypto-app.netlify.app/",
   "http://localhost:5173",
   "http://127.0.0.1:5173"
 ];
